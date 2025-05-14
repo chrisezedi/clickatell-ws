4ebdf5709088fe5ebe96be89a2a8f9bf68284781
chrisezedi <ekenechrisezedi@gmail.com>
2025-05-14 21:43:13 +0100

    feat: setup client server connection

--- apps/socket-server/src/main.ts
+++ apps/socket-server/src/main.ts
@@ -1,14 +1,25 @@
 import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
 
 const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
-const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send({ message: 'Hello API' });
+const server = createServer(app);
+const io = new Server(server, { cors: { origin:"http://localhost:4200" } });
+
+app.post('/activity', (req, res) => {
+  res.json({ message: req.body });
 });
 
-app.listen(port, host, () => {
+io.on('connection', (socket) => {
+  console.log('Client connected successfully:', socket.id);
+})
+
+server.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
